b9bbc09d2678ef182e7a40b00dae4f9865cebbee
zubr13 <andreizubritskiy@gmail.com>
2015-09-03 23:39:38 +0300

    Initial commit

---
+++ src/main/java/com/zubr/lib/MyFramework.java
@@ -0,0 +1,10 @@
+package com.zubr.lib;
+
+/**
+ * Created by andr on 03.09.15.
+ */
+public class MyFramework {
+    public static void makeAllProjects(){
+        System.out.println("Success!");
+    }
+}
